f0c5be93db85c33d7cf9d5214f335ac42e204702
jijiahui123 <2287898449@qq.com>
2020-06-02 16:30:33 +0800

    12

---
+++ common/src/main/java/com/ed/model/Tree.java
@@ -0,0 +1,15 @@
+package com.ed.model;
+
+import lombok.Data;
+
+@Data
+public class Tree {
+
+    private Integer id;
+
+    private  String name;
+    private  String number;
+
+    private String password;
+
+}
